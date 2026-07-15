48075943b85c9cca404292efb43d5ad9a414a5ac
PaulGDX <PGONG415@GMAIL.COM>
2026-07-16 07:34:02 +0800

    Initial Commit

---
+++ Inkling/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Inkling
+//
+//  Created by Paul Dexin Gong on 2026/7/16.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
